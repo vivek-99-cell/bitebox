790779196957f44860524f30c39718eaaac322c2
vivek-99-cell <tirkeyvivekpatrick@gmail.com>
2026-07-31 18:57:12 +0530

    new

---
+++ newclonebitebox/BiteBox/script.js
@@ -0,0 +1,23 @@
+function login()
+
+{
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let error = document.getElementById("error");
+
+    //simple condition
+    if(email==="admin@1234" && password === "1234"){
+
+        //save login state in localstorage
+        localStorage.setItem("user",email);
+        localStorage.setItem("userpassword",password);
+
+        //redirect
+        window.location.href = "Home.html";
+    }
+    else{
+        error.innerText = "Invalid email or password";
+    }
+}
